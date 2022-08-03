0dbb5b8bb731b66d3c183a023759af39e8395d00
Davner Firmino Cavalcante <davner23@gmail.com>
2022-08-03 16:59:52 -0400

    🪲Fix: corrigir bug do props

--- README.md
+++
@@ -1,1 +0,0 @@
-# aulas-de-react-udemy
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MeuProp from "./Componentes/MeuProp";
+React
+import "./index.css";
+
+function App() {
+  return <React.Fragment>
+   <MeuProp name="João" sobrename="Silva"/>
+ </React.Fragment>;
+}
+
+export default App;
