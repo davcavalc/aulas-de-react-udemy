8564b6832a719080383e6480df976c1c01396548
Davner Firmino Cavalcante <davner23@gmail.com>
2022-08-15 03:32:13 -0400

    📓Aula: Testando o Neovim - 15/08/2022

--- FORMULÁRIOS COM REACT/src/Componentes/Idade.jsx
+++ FORMULÁRIOS COM REACT/src/Componentes/Idade.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Idade = ({ nome }) => {
 	const geraSaudacao = (algumNome) => {
-		return `Olá ${algumNome}, tudo bem com você?`;
+		return `Olá ${algumNome}, tudo bem contigo ?`;
 	};
 	return <div className="bg-black text-amber-500 text-center">{geraSaudacao(nome)}</div>;
 };
