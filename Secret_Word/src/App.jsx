7786e72a53b815875ef9d35c955be5171acfbe64
Davner Firmino Cavalcante <davner23@gmail.com>
2022-08-17 02:22:58 -0400

    📓Aula: Secret  Word (parte 1) - 17/08/2022

---
+++ Secret_Word/src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="App uppercase font-bold">secret word</h1>
+      <div className="h-14 bg-gradient-r from-amber-500 to-amber-900 ">
+        <h2 className="text-center font-bold text-3xl hover:text-amber-600">Meu teste</h2>
+      </div>
+    </>
+  )
+}
+
+export default App;
