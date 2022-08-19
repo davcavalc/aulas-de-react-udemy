101bab0dd92a9e2177d6b67498299aff34f9bf26
Davner Firmino Cavalcante <davner23@gmail.com>
2022-08-19 15:46:35 -0400

    📓Aula: Secret World (parte 5) - 19/08/2022

--- Secret_Word/src/Componentes/Game.jsx
+++ Secret_Word/src/Componentes/Game.jsx
@@ -3,11 +3,31 @@ import React from "react";
 const Game = ({ verifyLetter }) => {
 	return (
 		<>
-			<div className="flex flex-col justify-center">
-                <h1>Game</h1>
-                <button className="btn" onClick={verifyLetter}>
-                    Finalizar o jogo
-                </button>
+            <div className="game">
+                <div className="points">
+                    <span>Pontuação: 000</span>
+                </div>
+                <h1>Advinhe a palavra:</h1>
+                <h3 className="tip">Dica sobre a palavra: <span>dica...</span></h3>
+                <div className="wordContainer">
+                    <span className="letter">A</span>
+                    <span className="blankSquare"></span>
+                </div>
+                <div className="letterContainer">
+                    <p>Tente advinhar uma letra da palavra</p>
+                    <form action="">
+                        <label className="flex flex-col gap-3">
+                            <input type="text" name="letter" maxLength={1} required />
+                            <button className="btn">Jogar</button>
+                        </label>
+                    </form>
+                </div>
+                <div className="wrongLettersContainer">
+                    <p>Letras já utilizadas</p>
+                    <span>b,</span>
+                    <span>a,</span>
+
+                </div>
             </div>
 		</>
 	);
