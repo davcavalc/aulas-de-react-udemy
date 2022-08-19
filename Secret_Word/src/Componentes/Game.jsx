c2a2f187e918c0f264f68fd9e8e1fc4015b13a91
Davner Firmino Cavalcante <davner23@gmail.com>
2022-08-19 16:15:20 -0400

    📓Aula: Secret World (parte 6) - 19/08/2022

--- Secret_Word/src/Componentes/Game.jsx
+++ Secret_Word/src/Componentes/Game.jsx
@@ -3,32 +3,32 @@ import React from "react";
 const Game = ({ verifyLetter }) => {
 	return (
 		<>
-            <div className="game">
-                <div className="points">
-                    <span>Pontuação: 000</span>
-                </div>
-                <h1>Advinhe a palavra:</h1>
-                <h3 className="tip">Dica sobre a palavra: <span>dica...</span></h3>
-                <div className="wordContainer">
-                    <span className="letter">A</span>
-                    <span className="blankSquare"></span>
-                </div>
-                <div className="letterContainer">
-                    <p>Tente advinhar uma letra da palavra</p>
-                    <form action="">
-                        <label className="flex flex-col gap-3">
-                            <input type="text" name="letter" maxLength={1} required />
-                            <button className="btn">Jogar</button>
-                        </label>
-                    </form>
-                </div>
-                <div className="wrongLettersContainer">
-                    <p>Letras já utilizadas</p>
-                    <span>b,</span>
-                    <span>a,</span>
-
-                </div>
-            </div>
+			<div className="game">
+				<div className="points">
+					<span className="pointContainer">Pontuação: 000</span>
+				</div>
+				<h1 className="title1">Advinhe a palavra:</h1>
+				<h3 className="tip">
+					Dica sobre a palavra: <span>dica...</span>
+				</h3>
+				<div className="wordContainer">
+					<span className="letter">A</span>
+					<span className="blankSquare"></span>
+				</div>
+				<div className="letterContainer">
+					<p>Tente advinhar uma letra da palavra</p>
+					<form action="">
+						<input type="text" name="letter" maxLength={1} required />
+						<input type="text" name="letter" maxLength={1} required />
+						<button className="btn">Jogar</button>
+					</form>
+				</div>
+				<div className="wrongLettersContainer">
+					<p>Letras já utilizadas</p>
+					<span>b,</span>
+					<span>a,</span>
+				</div>
+			</div>
 		</>
 	);
 };
