88bd8bca129f7dbb2b326d9cea15535966a7636e
Davner Firmino Cavalcante <davner23@gmail.com>
2022-08-21 22:15:28 -0400

    📓Aula: Secret World (parte 7) - 21/08/2022

--- Secret_Word/src/App.jsx
+++ Secret_Word/src/App.jsx
@@ -21,6 +21,11 @@ function App() {
 	const [pickedCategory, setPickedCategory] = useState("");
 	const [letters, setLetters] = useState([]);
 
+	const [guessedLetters, setGuessedLetters] = useState([]);
+	const [wrongLetters, setWrongLetters] = useState([]);
+	const [guesses, setGuesses] = useState(3);
+	const [score, setScore] = useState(0);
+
 	const pickWordAndCategory = () => {
 		// Pick a random category
 		const categories = Object.keys(words);
@@ -49,7 +54,7 @@ function App() {
 		// Fill states
 		setPickedWord(word);
 		setPickedCategory(category);
-		setLetters(letters);
+		setLetters(wordLetters);
 
 		setGameStage(stages[1].name);
 	};
@@ -67,7 +72,18 @@ function App() {
 		<>
 			<div className="App">
 				{gameStage === "start" && <StartScreen startGame={startGame} />}
-				{gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+				{gameStage === "game" && (
+					<Game
+						verifyLetter={verifyLetter}
+						pickedWord={pickedWord}
+						pickedCategory={pickedCategory}
+						letters={letters}
+						guessedLetters={guessedLetters}
+						wrongLetters={wrongLetters}
+						guesses={guesses}
+						score={score}
+					/>
+				)}
 				{gameStage === "end" && <GameOver retry={retry} />}
 			</div>
 		</>
--- Secret_Word/src/Componentes/Game.jsx
+++ Secret_Word/src/Componentes/Game.jsx
@@ -1,32 +1,48 @@
 import React from "react";
 
-const Game = ({ verifyLetter }) => {
+const Game = ({
+	verifyLetter,
+	pickedWord,
+	pickedCategory,
+	letters,
+	guessedLetters,
+	wrongLetters,
+	guesses,
+	score,
+}) => {
 	return (
 		<>
 			<div className="game">
 				<div className="points">
-					<span className="pointContainer">Pontuação: 000</span>
+					<span className="pointContainer">Pontuação: {score}</span>
 				</div>
 				<h1 className="title1">Advinhe a palavra:</h1>
 				<h3 className="tip">
-					Dica sobre a palavra: <span>dica...</span>
+					Dica sobre a palavra: <span>{pickedCategory}</span>
 				</h3>
+				<p>Você ainda tem {guesses} tentativa(s).</p>
 				<div className="wordContainer">
-					<span className="letter">A</span>
-					<span className="blankSquare"></span>
+					{letters.map((letter, i) =>
+						guessedLetters.includes(letter) ? (
+							<span key={i} className="letter">{letter}</span>
+						) : (
+							<span key={i} className="blankSquare"></span>
+						)
+					)}
 				</div>
 				<div className="letterContainer">
 					<p>Tente advinhar uma letra da palavra</p>
 					<form action="">
 						<input type="text" name="letter" maxLength={1} required />
-						<input type="text" name="letter" maxLength={1} required />
+
 						<button className="btn">Jogar</button>
 					</form>
 				</div>
 				<div className="wrongLettersContainer">
 					<p>Letras já utilizadas</p>
-					<span>b,</span>
-					<span>a,</span>
+					{wrongLetters.map((letter, i) => (
+						<span key={i}>{letter}, </span>
+					))}
 				</div>
 			</div>
 		</>
