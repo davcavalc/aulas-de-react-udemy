8d4919a0ef5fa0e73407ab9eda467a138fda65ad
Davner Firmino Cavalcante <davner23@gmail.com>
2022-08-25 21:14:13 -0400

    📓Aula: Processando letras válidas e inválidas - 25/08/2022

--- Secret_Word/src/App.jsx
+++ Secret_Word/src/App.jsx
@@ -61,9 +61,22 @@ function App() {
 
 	// Process the letter input
 	const verifyLetter = (letter) => {
-		console.log(letter);
-	};
+		const normalizedLetter = letter.toLowerCase();
+		// Check if letter has already been utilized
+		if (guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter)) {
+			return;
+		}
+		// push guessedLetter or remove a guess
+		if (letters.includes(normalizedLetter)) {
+			setGuessedLetters((actualGuessedLetters) => [...actualGuessedLetters, normalizedLetter]);
+		} else {
+			setWrongLetters((actualWrongLetters) => [...actualWrongLetters, normalizedLetter]);
+		}
 
+	};
+	
+	console.log(guessedLetters);
+	console.log(wrongLetters);
 	// Restarts the game
 	const retry = () => {
 		setGameStage(stages[0].name);
