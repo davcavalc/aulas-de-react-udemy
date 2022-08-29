01cb55a07700aaa4264c6dd041af749ef32041a3
Davner Firmino Cavalcante <davner23@gmail.com>
2022-08-28 20:50:15 -0400

    📓Aula: Finalizando a tela do fim de jogo - 28/08/2022

--- Secret_Word/src/App.jsx
+++ Secret_Word/src/App.jsx
@@ -114,7 +114,7 @@ function App() {
 						score={score}
 					/>
 				)}
-				{gameStage === "end" && <GameOver retry={retry} />}
+				{gameStage === "end" && <GameOver retry={retry} score={score} />}
 			</div>
 		</>
 	);
--- Secret_Word/src/Componentes/GameOver.jsx
+++ Secret_Word/src/Componentes/GameOver.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-const GameOver = ({ retry }) => {
+const GameOver = ({ retry, score }) => {
 	return (
 		<>
-            <div className="flex flex-col justify-center">
-                {""}
-				<h1>Game Over</h1>
+			<div className="flex flex-col justify-center">
+				{""}
+				<h1>Fim de jogo!!</h1>
+				<p>
+					A sua pontuação foi: <span className="text-[#ecfa00] text-[1.5em]">{score}</span>
+				</p>
 				<button className="btn" onClick={retry}>
 					Resetar o jogo
 				</button>
