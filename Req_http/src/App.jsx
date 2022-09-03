6f58196e58c011f41cd0e0ac40fb38c9a28e69f5
Davner Firmino Cavalcante <davner23@gmail.com>
2022-09-03 01:06:31 -0400

    📓Aula: Adicionando dados com React - 03/09/2022

--- Req_http/src/App.jsx
+++ Req_http/src/App.jsx
@@ -6,6 +6,8 @@ import { useState, useEffect } from "react";
 
 function App() {
  const [products, setProducts] = useState([]);
+ const [name, setName] = useState("");
+ const [price, setPrice] = useState("");
 
  // 1 - resgatando dados
  useEffect(() => {
@@ -20,6 +22,22 @@ function App() {
   fetchData();
  }, []);
 
+ // 2 - Add de produtos
+ const handleSubmit = async (e) => {
+  e.preventDefault();
+  const product = {
+   name,
+   price,
+  };
+  const res = await fetch("http://localhost:3000/products", {
+   method: "POST",
+   headers: {
+    "Content-type": "application/json",
+   },
+   body: JSON.stringify(product),
+  });
+ };
+
  return (
   <>
    <div className="border-[4px] border-black shadow-lg shadow-blue-500">
@@ -31,6 +49,37 @@ function App() {
       </li>
      ))}
     </ul>
+    {/* FORMULÁRIO */}
+    <div className="add-product">
+     <form
+      className="flex flex-col items-center justify-center"
+      onSubmit={handleSubmit}
+     >
+      <label htmlFor="">
+       <span>Nome:</span>
+       <input
+        className="flex flex-col mb-[15px]"
+        type="text"
+        value={name}
+        name="name"
+        onChange={(e) => setName(e.target.value)}
+       />
+       <span>Preço:</span>
+       <input
+        className="flex flex-col mb-[15px]"
+        type="number"
+        value={price}
+        name="price"
+        onChange={(e) => setPrice(e.target.value)}
+       />
+       <input
+        className="mb-4 border-[2px] border-[#000] w-40 rounded-md shadow-lg shadow-black font-bold hover:border-[6px] hover:border-amber-500 hover:bg-black hover:text-white"
+        type="submit"
+        value="Criar produto"
+       />
+      </label>
+     </form>
+    </div>
    </div>
   </>
  );
