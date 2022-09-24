427747eae3efe006339e7fb4976d369fa4ca08c7
Davner Firmino Cavalcante <davner23@gmail.com>
2022-09-23 21:54:38 -0400

    📓Aula: Link ativo - 23/09/2022

--- React_Router/src/Componentes/Navbar.jsx
+++ React_Router/src/Componentes/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 function Navbar() {
  return (
   <nav className="flex flex-row justify-center font-bold  gap-2">
-   <Link className="hover:border-[1px] hover:bg-amber-500 rounded-md mb-4 w-[5em] text-center hover:border-black" to="/">Home</Link>
+   {/* <Link className="hover:border-[1px] hover:bg-amber-500 rounded-md mb-4 w-[5em] text-center hover:border-black" to="/">Home</Link>
    <Link className="hover:border-[1px] hover:bg-amber-500 rounded-md mb-4 w-[5em] text-center hover:border-black" to="/about">About</Link>
-   <Link className="hover:border-[1px] hover:bg-amber-500 rounded-md mb-4 w-[5em] text-center hover:border-black" to="/products">Products</Link>
+   <Link className="hover:border-[1px] hover:bg-amber-500 rounded-md mb-4 w-[5em] text-center hover:border-black" to="/products">Products</Link> */}
+   <NavLink to="/" className={({isActive}) => (isActive ? "está ativo" : "não está ativo")} >Home</NavLink>
+   <NavLink to="/about">About</NavLink>
+   <NavLink to="/products">Product</NavLink>
   </nav>
  );
 }
