206e502a1995954cfde2aa1b9cef80a4fe26d8f5
Davner Firmino Cavalcante <davner23@gmail.com>
2022-09-29 21:38:39 -0400

    💽 Seção 9: Context API - 29/09/2022

---
+++ Context_API/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="flex justify-center text-3xl mt-4 uppercase font-bold hover:underline hover:bg-black hover:text-amber-500">
+        Context API
+      </h1>
+    </>
+  )
+}
+
+export default App
