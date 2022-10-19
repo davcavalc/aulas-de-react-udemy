a91b24aff0ff4e86d0e5c9511955e96de9817f33
Davner Firmino Cavalcante <davner23@gmail.com>
2022-10-19 01:42:23 -0400

    📓Aula: Criando contextos complexos (início da correção) - 19/10/2022

--- Context_API/src/main.jsx
+++ Context_API/src/main.jsx
@@ -3,15 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { CounterContextProvider } from "./context/CounterContext.jsx";
-import { TitleColorContextProvider } from "./context/TitleColorContext";
+// import { TitleColorContextProvider } from "./context/TitleColorContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
  <React.StrictMode>
   {/* 2 - CRIANDO PROVIDER */}
   <CounterContextProvider>
-   <TitleColorContextProvider>
     <App />
-   </TitleColorContextProvider>
+   {/* <TitleColorContextProvider></TitleColorContextProvider> */}
   </CounterContextProvider>
  </React.StrictMode>
 );
--- Context_API/src/pages/Inicio.jsx
+++ Context_API/src/pages/Inicio.jsx
@@ -4,19 +4,20 @@ import ChangeCounter from "../Componentes/ChangeCounter.jsx";
 // import { CounterContext } from "../context/CounterContext.jsx";
 
 // 5 - CONTEXT MAIS COMPLEXO
-import { useTitleColorContext } from "../hooks/useTitleColorContext.jsx";
+// import { useTitleColorContext } from "../hooks/useTitleColorContext.jsx";
 
 // 4 - REFATORANDO COM HOOK
 import { useCounterContext } from "../hooks/useCounterContext.jsx";
 const Inicio = () => {
-//  const { counter } = useContext(CounterContext);
-    const { counter } = useCounterContext();
+ //  const { counter } = useContext(CounterContext);
+ const { counter } = useCounterContext();
 
-    // 5 - context mais complexo
-    const { color } = useTitleColorContext();
+ // 5 - context mais complexo
+ // const { color } = useTitleColorContext();
  return (
   <>
-   <h1 style={{color: color}}>Home</h1>
+   {/* <h1 style={{color: color}}>Home</h1> */}
+   <h1>Home</h1>
    <p>Valor do contador: {counter}</p>
    {/* 3 - ALTERANDO O VALOR DO CONTEXTO */}
    <ChangeCounter />
